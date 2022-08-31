10dadcfee7164894d80060df9b3681177611c06c
x2Dzi <bojan.dukovski211@gmail.com>
2022-09-01 01:46:07 +0200

    pictures api

--- src/main/java/personal/website/backend/config/DataInitializer.java
+++ src/main/java/personal/website/backend/config/DataInitializer.java
@@ -2,27 +2,30 @@ package personal.website.backend.config;
 
 import org.springframework.stereotype.Component;
 import personal.website.backend.model.Picture;
+import personal.website.backend.repository.PictureRepository;
 import personal.website.backend.service.PictureService;
 
 import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
 import java.io.File;
 
 @Component
 public class DataInitializer {
-    private final PictureService pictureService;
+    private final PictureRepository pictureRepository;
 
-    public DataInitializer(PictureService pictureService) {
-        this.pictureService = pictureService;
+    public DataInitializer(PictureRepository pictureRepository) {
+        this.pictureRepository = pictureRepository;
     }
 
     @PostConstruct
-    public void init() {
+    @Transactional
+    public void initData() {
         File folder = new File("C:\\Users\\Bojan\\Documents\\slikiZaEditiranje\\marjaIVic05.05odbrani");
         System.err.println(folder.getAbsolutePath());
         for (File f : folder.listFiles()) {
             File file = new File(f.getAbsolutePath());
             Picture picture = new Picture(file.getName(), file.getAbsolutePath());
-            this.pictureService.save(picture);
+            this.pictureRepository.save(picture);
         }
     }
 }
--- src/main/java/personal/website/backend/model/Picture.java
+++ src/main/java/personal/website/backend/model/Picture.java
@@ -1,12 +1,18 @@
 package personal.website.backend.model;
 
+
 import lombok.Data;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Picture {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String url;

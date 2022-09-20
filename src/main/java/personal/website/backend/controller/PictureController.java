889d55dafba847943c59f04ad3a16a83ac7d4bbd
x2Dzi <bojan.dukovski211@gmail.com>
2022-09-20 21:02:37 +0200

    design and pictures feed + uploading pictures

--- src/main/java/personal/website/backend/controller/PictureController.java
+++ src/main/java/personal/website/backend/controller/PictureController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://bojandukovski.herokuapp.com")
+@CrossOrigin(origins = "http://bojandukovski.herokuapp.com")
 public class PictureController {
 
     private final PictureService pictureService;
